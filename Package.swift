9540a2659c3a23805ecb430ebd75a99b943000e9
usagimaru <usagimaruma@gmail.com>
2022-02-23 04:15:59 +0900

    Trim the space from the library name.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 	products: [
 		// Products define the executables and libraries a package produces, and make them visible to other packages.
 		.library(
-			name: " FittingTextField",
+			name: "FittingTextField",
 			targets: ["FittingTextField"]),
 	],
 	dependencies: [
